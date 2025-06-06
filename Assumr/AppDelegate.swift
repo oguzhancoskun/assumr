4c6c059dad8907c93477ea6e90f20bb45ac07124
elgormus <elgormus@freelancer.run>
2025-06-06 14:16:52 +0100

    Initial Commit

---
+++ Assumr/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Assumr
+//
+//  Created by elgormus on 06/06/2025.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
